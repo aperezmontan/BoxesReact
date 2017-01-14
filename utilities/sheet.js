4f42c7dd2c27a2b9bbeefa063449e6ab46e57b49
Ari Perez <aris.a.perez@gmail.com>
2017-01-14 06:51:57 -0700

    Creates usable grid of boxes

--- utilities/sheet.js
+++ utilities/sheet.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Dimensions,
   ScrollView,
   StyleSheet,
   Text,
@@ -27,9 +28,12 @@ var Sheet = React.createClass({
     api = this.props.route.api;
     Api(api.route, api.params, api.body)
     .then((response) => {
-      this.setState({
-        sheets: response.sheets
-      })
+      if(response){
+        this.setState({
+          response: response,
+          sheets: response.sheets
+        })
+      }
     })
   },
   border(color){
@@ -40,12 +44,14 @@ var Sheet = React.createClass({
   },
   render() {
     var navigator = this.props.navigator;
-    var route = this.props.route
-    var game = route.game
-    var sheet_rows = []
+    var route = this.props.route;
+    var game = route.game;
+    var sheet_rows = [];
 
 
+    console.log(this.state.sheets);
     console.log(route.title);
+    console.log(this.state.response);
     if (route.title === 'createSheet'){
       return (
       <View style={[styles.container, this.border('black')]}>
@@ -96,7 +102,8 @@ var Sheet = React.createClass({
         </View>
       );
     } else {
-      var sheet = this.state.sheets
+      var sheet = this.state.sheets[0]
+      console.log("sheet: ",sheet);
       return (
         <View style={[styles.container, this.border('black')]}>
           <SheetView sheet={sheet}/>
@@ -107,29 +114,84 @@ var Sheet = React.createClass({
 })
 
 var BoxView = React.createClass({
+  getInitialState(){
+    return {
+      away_team_id: this.props.away_coord,
+      home_team_id: this.props.home_coord,
+    };
+  },
   border(color){
     return {
-      borderColor: color,
-      borderWidth: 1,
-      borderRadius: 4,
+      borderColor: 'white',
+      borderWidth: .5,
     }
   },
-  onPress() {
+  onPress(){
     alert("YO FROM RIGHT BUTTON")
   },
   selectBox(away_team_id, home_team_id){
     console.log(away_team_id, home_team_id)
   },
   render(){
-    away_team_id = this.props.away_coord;
-    home_team_id = this.props.home_coord;
+    color = this.props.boxColor;
     return(
-      <View style={[styles.boxContainer, this.border('black')]}>
-        <TouchableHighlight onPress={() => this.selectBox(away_team_id, home_team_id)}>
-          <View style={styles.scrollView}>
-          </View>
-        </TouchableHighlight>
-      </View>
+      <TouchableHighlight onPress={() => this.selectBox(this.state.away_team_id, this.state.home_team_id)} style={[{flex: 1, backgroundColor: color}, this.border('black')]}>
+        <Text style={{fontFamily: 'Montserrat', textAlign: 'center'}}>BOX</Text>
+      </TouchableHighlight>
+    )
+  }
+})
+
+var AwayNumView = React.createClass({
+  border(color){
+    return {
+      borderColor: color,
+      borderWidth: 1,
+    }
+  },
+  render(){
+    away_team_id = this.props.away_coord;
+    color = 'white';
+    return(
+      <TouchableHighlight style={{flex: 1, backgroundColor: color}}>
+        <Text style={{fontFamily: 'Montserrat', textAlign: 'center'}}>{away_team_id}</Text>
+      </TouchableHighlight>
+    )
+  }
+})
+
+var HomeNumView = React.createClass({
+  border(color){
+    return {
+      borderColor: color,
+      borderWidth: 1,
+    }
+  },
+  render(){
+    home_team_id = this.props.home_coord;
+    color = 'white';
+    return(
+      <TouchableHighlight style={{flex: 1, backgroundColor: color}}>
+        <Text style={{fontFamily: 'Montserrat', textAlign: 'center'}}>{home_team_id}</Text>
+      </TouchableHighlight>
+    )
+  }
+})
+
+var GameCornerBox = React.createClass({
+    border(color){
+    return {
+      borderColor: color,
+      borderWidth: 1,
+    }
+  },
+  render(){
+    game = this.props.game;
+    color = 'white';
+    return(
+      <TouchableHighlight style={{flex: 1, backgroundColor: color}}>
+        <Text style={{fontFamily: 'Montserrat', textAlign: 'center'}}>{game}</Text>
+      </TouchableHighlight>
     )
   }
 })
@@ -142,28 +204,63 @@ var SheetView = React.createClass({
     }
   },
   render(){
-    var away_team = this.props.sheet.away_team;
-    var home_team = this.props.sheet.home_team;
+    var sheet = this.props.sheet;
+  console.log("props in Sheetview: ", sheet)
+    // var away_team = this.props.sheet.away_team;
+    // var home_team = this.props.home_team;
+    // console.log({this.props.sheet.home_team})
     var home_coords = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
     var away_coords = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-    var boxes = [];
+    var sheetRow = [];
+    var sheetRows = [];
+    var windowHeight = (Dimensions.get('window').height - iosHeight)/11;
+    var windowWidth = Dimensions.get('window').width;
+    var iosHeight = 64; //android is 54
+    var boxHeight = windowHeight/11;
+    var colors = ['powderblue', 'skyblue', 'steelblue'];
+    var game = "game";
+
+    sheetRow.push(<GameCornerBox game={game} key={game}/>)
+
+    away_coords.forEach(function(away_coord){
+      key = 'away' + away_coord
+      sheetRow.push(<AwayNumView away_coord={away_coord} key={key}/>)
+    })
+
+    rowKey = "awayRowNums"
+    sheetRows.push(
+      <View key={rowKey} style={{flex: 1, flexDirection: 'row', height: boxHeight, width: windowWidth}}>
+        {sheetRow}
+      </View>
+    );
+    sheetRow = [];
+
     home_coords.forEach(function(home_coord){
+      key = 'home' + home_coord
+      rowKey = 'homeRow' + home_coord
+      sheetRow.push(<HomeNumView home_coord={home_coord} key={key}/>)
+      var i = 0;
       away_coords.forEach(function(away_coord){
-        key = away_coord+home_coord
-        boxes.push(<BoxView home_coord={home_coord} away_coord={away_coord} key={key}/>)
+        if (i == 3){
+          i = 0;
+        }
+        color = colors[i];
+        key = away_coord+home_coord;
+        sheetRow.push(<BoxView home_coord={home_coord} away_coord={away_coord} boxColor={color} key={key}/>)
+        i = i + 1;
       });
+      sheetRows.push(
+        <View key={rowKey} style={{flex: 1, flexDirection: 'row', height: boxHeight, width: windowWidth}}>
+          {sheetRow}
+        </View>
+      );
+      sheetRow = [];
     });
     return(
       <View style={[styles.scrollViewContainer, this.border('black')]}>
-        <ScrollView
-          style={styles.scrollView}
-          maximumZoomScale = {3}
-          minimumZoomScale = {1}
-        >
-          <View style={[styles.sheetContainer, this.border('black')]}>
-            {boxes}
-          </View>
-        </ScrollView>
+      <ScrollView style={[{flexDirection: 'column'}, styles.scrollView]} maximumZoomScale = {3} minimumZoomScale = {1}>
+        {sheetRows}
+      </ScrollView>
       </View>
     )
   }
@@ -177,7 +274,7 @@ var SheetListView = React.createClass({
     this.props.navigator.push({
       title: sheet.name,
       component: Sheet,
-      sheet: sheet,
+      sheet: sheet, //this allows me to call sheet[0] because there's only one once it's been selected
       onPress: this.onPress,
       rightText: 'ALERT!',
       api:{
@@ -208,11 +305,11 @@ var SheetListView = React.createClass({
 
 const styles = StyleSheet.create({
   boxContainer: {
-    flex: 1,
-    backgroundColor: '#CCC',
-    // margin: 10,
-    width: 35,
-    height: 35
+    // flex: 1,
+    // backgroundColor: '#CCC',
+    // // margin: 10,
+    // width: 35,
+    // height: 35
   },
   boxText:{
     justifyContent: 'center',
@@ -253,3 +350,66 @@ const styles = StyleSheet.create({
 });
 
 module.exports = Sheet;
+
+// var BoxView = React.createClass({
+//   border(color){
+//     return {
+//       borderColor: color,
+//       borderWidth: 1,
+//       borderRadius: 4,
+//     }
+//   },
+//   onPress() {
+//     alert("YO FROM RIGHT BUTTON")
+//   },
+//   selectBox(away_team_id, home_team_id){
+//     console.log(away_team_id, home_team_id)
+//   },
+//   render(){
+//     away_team_id = this.props.away_coord;
+//     home_team_id = this.props.home_coord;
+//     return(
+//       <View style={[styles.boxContainer, this.border('black')]}>
+//         <TouchableHighlight onPress={() => this.selectBox(away_team_id, home_team_id)}>
+//           <View style={styles.scrollView}>
+//           </View>
+//         </TouchableHighlight>
+//       </View>
+//     )
+//   }
+// })
+
+// var SheetView = React.createClass({
+//   border(color){
+//     return {
+//       // borderColor: color,
+//       // borderWidth: 4,
+//     }
+//   },
+//   render(){
+//     var away_team = this.props.sheet.away_team;
+//     var home_team = this.props.sheet.home_team;
+//     var home_coords = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+//     var away_coords = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+//     var boxes = [];
+//     home_coords.forEach(function(home_coord){
+//       away_coords.forEach(function(away_coord){
+//         key = away_coord+home_coord
+//         boxes.push(<BoxView home_coord={home_coord} away_coord={away_coord} key={key}/>)
+//       });
+//     });
+//     return(
+//       <View style={[styles.scrollViewContainer, this.border('black')]}>
+//         <ScrollView
+//           style={styles.scrollView}
+//           maximumZoomScale = {3}
+//           minimumZoomScale = {1}
+//         >
+//           <View style={[styles.sheetContainer, this.border('black')]}>
+//             {boxes}
+//           </View>
+//         </ScrollView>
+//       </View>
+//     )
+//   }
+// })
