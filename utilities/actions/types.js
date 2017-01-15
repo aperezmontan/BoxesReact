4fe3c50a800c3264d1c0f58753e6a5aa42f9194b
Ari Perez <aris.a.perez@gmail.com>
2017-01-15 14:21:30 -0700

    Create actions folder with all the actions I can think of right now

---
+++ utilities/actions/types.js
@@ -0,0 +1,17 @@
+// Navigation actions
+export const CHANGE_TAB = 'CHANGE_TAB';
+
+// API actions
+export const FETCH_GAMES = 'FETCH_GAMES';
+export const FETCH_GAMES_COMPLETE = 'FETCH_GAMES_COMPLETE';
+export const FETCH_SHEETS = 'FETCH_SHEETS';
+export const FETCH_SHEETS_COMPLETE = 'FETCH_SHEETS_COMPLETE';
+export const FETCH_SHEET = 'FETCH_SHEET';
+export const FETCH_SHEET_COMPLETE = 'FETCH_SHEET_COMPLETE';
+export const FETCH_USER = 'FETCH_USER';
+export const FETCH_USER_COMPLETE = 'FETCH_USER_COMPLETE';
+
+// Box actions
+export const SELECT_BOX = 'SELECT_BOX';
+export const DESELECT_BOX = 'DESELECT_BOX';
+export const CONFIRM_BOX = 'CONFIRM_BOX';
